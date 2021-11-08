a0a199ba7e0b97102b56461209a85a95fb26ef64
mjdean1994 <mjdean1994@gmail.com>
2021-11-08 09:16:21 -0500

    Fixing flow data integrity issue

--- flowHandlers/dmHandler.js
+++ flowHandlers/dmHandler.js
@@ -1,3 +1,4 @@
+const characterData = require('../data/characterData')
 const flowData = require('../data/flowData')
 const logger = require('../lib/logger')
 
@@ -7,14 +8,24 @@ module.exports.handle = (message) => {
             message.author.send("Failed to get flowstate: " + err);
             return
         }
-
-        if (!state) {
-            state = 'new'
-        }
-        try {
-            require(`./${state}`)(message)
-        } catch (ex) {
-            logger.error(`Could not find DM handler for state "${state}": ${ex}.`)
-        }
+        characterData.getByDiscordId(message.author.id, (err, character) => {
+            if (err) {
+                message.author.send("Failed to get character information during DM handling: " + err)
+                return;
+            }
+            if (!state) {
+                if (character) {
+                    logger.warn(`Couldn't find a flow state for user ${message.author.id} but they have a character!`)
+                    state = 'idle'
+                } else {
+                    state = 'new'
+                }
+            }
+            try {
+                require(`./${state}`)(message)
+            } catch (ex) {
+                logger.error(`Could not find DM handler for state "${state}": ${ex}.`)
+            }
+        })
     })
 }
